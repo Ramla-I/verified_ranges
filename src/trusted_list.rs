9bf8b12e4b5c5a8e00479293a8124f6c35435b40
ramla <ijazramla@gmail.com>
2022-10-20 16:20:18 -0400

    can't seem to verify quantifers where the indexing expression is different than in the spec

--- src/trusted_list.rs
+++ src/trusted_list.rs
@@ -35,7 +35,7 @@ impl TrustedList {
     }
 
     pub fn push_unique(&mut self, elem: FrameRange) -> Option<FrameRange> {
-        if self.object_overlaps_in_list(&elem) {
+        if true { //self.object_overlaps_in_list(&elem) {
             Some(elem)
         } else {
             self.push(elem);
@@ -44,34 +44,66 @@ impl TrustedList {
 
     }
 
-    fn object_in_list(&self, obj: &FrameRange) -> (result: bool) 
+    fn object_in_list_rec(&self, start_index: usize, obj: &FrameRange) -> (result: Option<usize>) 
         requires 
-            self.list.len() > 0
+            self.list.len() > 0,
+            start_index < self.list.len(),
+            start_index >= 0
         ensures
-            result ==> exists|i:int| #![trigger self.list[i]] 0 <= i < self.list.len() ==> self.list[i].0.start == obj.0.start
+            result.is_Some() ==> (self.list[result.get_Some_0() as int].0.start == obj.0.start) && (self.list[result.get_Some_0() as int].0.end == obj.0.end),
+            // result.is_None() ==> forall|i: int| 
+            //     #![trigger obj.0, self.list@.index(i)]
+            //     0 <= i <= start_index ==> (self.list@.index(i).0.start != obj.0.start) || (self.list@.index(i).0.end != obj.0.end),
+        decreases start_index
     {
-        let mut i = 0;
-        while i < self.list.len() {
-            let list_obj = self.list.index(i);
-            if (obj.0.start == list_obj.0.start) && (obj.0.end == list_obj.0.end) {
-                return true;
-            }
-            i = i + 1;
+        // let list_obj = self.list@.index(start_index);
+        if (obj.0.start == self.list.index(start_index).0.start) && (obj.0.end == self.list.index(start_index).0.end) {
+            return Some(start_index);
         }
-        false
+
+        if start_index == 0 {
+            return None;
+        }
+
+        return self.object_in_list_rec(start_index - 1, obj);
     }
 
-    fn object_overlaps_in_list(&self, obj: &FrameRange) -> (result: bool)
+    fn object_in_list(&self, obj: &FrameRange) -> (result: Option<usize>) 
+        ensures
+            result.is_Some() ==> (self.list[result.get_Some_0() as int].0.start == obj.0.start) && (self.list[result.get_Some_0() as int].0.end == obj.0.end),
+            // result.is_None() ==> forall|i: int| 
+            //     0 <= i < self.list.len() ==> (self.list[i].0.start != obj.0.start) || (self.list[i].0.end != obj.0.end),
     {
         let mut i = 0;
         while i < self.list.len() {
-            let list_obj = self.list.index(i);
-            if obj.overlaps(list_obj) {
-                return true;
+            if (self.list.index(i).0.start == obj.0.start) && (self.list.index(i).0.end == obj.0.end) {
+                return Some(i);
             }
-            i = i + 1;
+            i = i+1;
         }
-        false
+        None
     }
+    // proof fn object_overlaps_in_list(&self, start_index: int, obj: &FrameRange) -> (result: Option<int>) 
+    //     requires 
+    //         self.list.len() > 0,
+    //         start_index <= self.list.len(),
+    //         start_index >= 0
+    //     ensures
+    //         result.is_Some() ==> self.list[result.get_Some_0()].0.start == obj.0.start
+    //     decreases start_index
+    // {
+    //     let list_obj = self.list@.index(start_index);
+    //     if (obj.0.start == list_obj.0.start) && (obj.0.end == list_obj.0.end) {
+    //         return Some(start_index);
+    //     }
+
+    //     if start_index == 0 {
+    //         return None;
+    //     }
+
+    //     return self.object_in_list(start_index - 1, obj);
+    // }
 }
+
+
 }
